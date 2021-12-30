2a29c23d585d2d876c8e1625461a0080bd097710
cristianoag@hotmail.com <Guilherme123>
2021-12-30 19:13:38 -0300

    Sync using JIFFY method

--- projects/Snake/Snake/targetconfig.h
+++ projects/Snake/Snake/targetconfig.h
@@ -1,6 +1,6 @@
 //-------------------------------------------------	
 // targetconfig.h created automatically by make.bat	
-// on 18:47:03.65 , Thu 12/30/2021 				
+// on 19:00:21.59 , Thu 12/30/2021 				
 //														
 // DO NOT BOTHER EDITING THIS.							
 // ALL CHANGES WILL BE LOST.							
