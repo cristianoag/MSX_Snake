0130ec88e5208ba4a13ac5566075a0fa4f825db6
cristianoag@hotmail.com <Guilherme123>
2021-12-30 21:29:45 -0300

    Session 7 - Code to show start and game over screens in the right way. Code to make the snake move continuously. Code to read the keyboard every cycle and take action each 15 cycles. Fix to avoid pushing opposite direction.

--- projects/Snake/Snake/msxromapp.c
+++ projects/Snake/Snake/msxromapp.c
@@ -1,23 +1,18 @@
-// ----------------------------------------------------------
-//		msxromapp.c - by Danilo Angelo, 2020
-//
-//		ROM program(cartridge) for MSX example
-//		C version
-// ----------------------------------------------------------
+//snake game for msx
 
 #include <stdbool.h>
-#include "targetconfig.h"
-#include "MSX/BIOS/msxbios.h"
-#include "msx_fusion.h"
-#include "screens.h"
+#include "targetconfig.h" 
+#include "MSX/BIOS/msxbios.h" //bios definitions
+#include "msx_fusion.h" //fusion c
+#include "screens.h" //game screens
 
-#define NAMETABLE	0x1800
+#define NAMETABLE	0x1800  //default initial address for the nametable (6144 in decimal)
 
-unsigned char x, y; //snake coordinates
+unsigned char x, y, direction; //snake coordinates and direction
 unsigned char joy; //joystick value
 bool EoG; //end of game control
-unsigned char content;
-unsigned int lastJiffy;
+unsigned char content; //used to get the current position of the snake
+unsigned int lastJiffy; //stores the last bios JIFFY
 
 // ----------------------------------------------------------
 //	This is an example of embedding asm code into C.
@@ -54,10 +49,7 @@ __endasm;
 	return;
 }
 
-// ----------------------------------------------------------
-//	This is an example of using debug code in C.
-//	This is only for the demo app.
-//	You can safely remove it for your application.
+// print function
 void print(char* msg) {
 #ifdef DEBUG
 	_print("[DEBUG]");
@@ -66,21 +58,26 @@ void print(char* msg) {
 	return;
 }
 
+//prints the game title screen
 void title() {
-	Cls();
-	print(titleScreen);
-	InputChar();
+	Cls(); //clear the screen
+	print(titleScreen); //print the title screen
+	while (Inkey() > 0) {}
+	InputChar(); //wait for a key
 }
 
+//main game routine
 void game() {
-	Cls();
-	print(gameScreen);
+	Cls(); //clear the screen
+	print(gameScreen); //print the game screen
 
 	// Initialize game variables
-	x = 10;
-	y = 10;
-	EoG = false;
-	Pokew(BIOS_JIFFY, 0);
+	x = 10; //initial snake position x
+	y = 10; //initial snake position y
+	direction = RIGHT; //starting direction is right
+
+	EoG = false; //the game is starting
+	Pokew(BIOS_JIFFY, 0); //initialize JIFFY to 0
 
 	// Game's main loop
 	while (!EoG) {
@@ -88,13 +85,22 @@ void game() {
 		//wait until BIOS changes the value of Jiffy
 		while (lastJiffy == Peekw(BIOS_JIFFY)) {}
 		
-		//from this point, 1 pass per frame
+		//read the keyboard
+		joy = JoystickRead(CURSORKEYS); //read the keyboard value
+		//logic to avoid the issue with pushing opposite direction
+		if (((joy == UP) && (direction != DOWN)) || 
+			((joy == RIGHT) && (direction != LEFT)) || 
+			((joy == DOWN) && (direction != UP)) || 
+			((joy == LEFT) && (direction != RIGHT)))
+		{
+			direction = joy;
+		}
+
+		//from this point, 1 pass each 15 frames
 		if (Peekw(BIOS_JIFFY) == 15)
 		{
-
-			joy = JoystickRead(CURSORKEYS); //read the joystick value
-
-			switch (joy) {
+			//check the direction being pushed
+			switch (direction) {
 			case UP:
 				y--;
 				break;
@@ -109,39 +115,50 @@ void game() {
 				break;
 			}
 
+			//get the current position of the snake
 			content = Vpeek(NAMETABLE + y * 32 + x);
-			EoG = ((joy > 0) && (content != ' '));
+			//check if the snake is being moved by the user and if it is not colliding
+			EoG = (content != ' ');
+			
+			//move the snake and print
 			Locate(x, y);
 			print("*");
 
+			//reset the JIFFY to 0
 			Pokew(BIOS_JIFFY, 0);
 		}
 
-		//sound routinne (1 execution per frame)
+		//from this point, 1 pass per frame
 		{}
 
+		//get the current JIFFY and put it on lastJiffy
 		lastJiffy = Peekw(BIOS_JIFFY);
 	}
 
-	InputChar();
+	Beep();
+	Pokew(BIOS_JIFFY, 0); //reset bios jiffy
+	while (Peek(BIOS_JIFFY) < 40) {} //wait 40 cycles
 }
 
+//game over routine
 void gameOver() {
 	Locate(0, 10);
-	print(gameOverMsg);
-	InputChar();
+	print(gameOverMsg); //print the game over message
+	while (Inkey() > 0) {} 
+	InputChar(); //wait for the user
 }
 
+//main loop for the game
 void main(void) {
-	Screen(1);
-	Width(32);
-	SetColors(255, 0, 0);
+	Screen(1); //sets screen mode 1
+	Width(32); //ensures 32 columns
+	SetColors(255, 0, 0); //set the color for the screen 
 	
 	//game infinite loop
 	while (true) {
-		title();
-		game();
-		gameOver();
+		title(); //title
+		game(); //game
+		gameOver(); //game over
 	}
 }
 
